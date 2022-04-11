f4e8d5aa9869eb2697056fec5ca91123142eda7b
brunogrecco <bgrecco@udesa.edu.ar>
2022-04-11 09:50:07 -0300

    css

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -36,7 +36,7 @@ showDescription(){
                 <i className="far fa-window-close" 
                 onClick={()=>this.props.delete(this.props.trackDetail.id)}></i>
             </section>
-            <main>
+            <main class="imagen">
                 <img src={this.props.trackDetail.album.cover} alt=""/>
                 <h3>{this.props.trackDetail.title}</h3>
                 <p className="description">{this.props.trackDetail.artist.name}</p>
